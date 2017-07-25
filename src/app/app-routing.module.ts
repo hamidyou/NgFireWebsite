46d26f2371bfc9b2d9c9bfd1d6b7f6af780d95c8
hamidyou <yhamid1982@gmail.com>
2017-07-24 22:47:06 -0400

    Unit One & Piano Lesson Routes Working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import {UnitOneComponent} from './unit-one/unit-one.component';
+import {PianoLessonComponent} from './unit-one/piano-lesson/piano-lesson.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent
+  },
+  {
+    path: 'unit-one',
+    component: UnitOneComponent
+  },
+  {
+    path: 'unit-one/piano-lesson',
+    component: PianoLessonComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {AngularFireModule} from 'angularfire2';
 import {environment} from '../environments/environment';
 import {AngularFireAuthModule} from 'angularfire2/auth';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {HeaderComponent} from './header/header.component';
 import {AuthenticationService} from './authentication.service';
-import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import {UserRegistrationComponent} from './user-registration/user-registration.component';
 import {FormsModule} from '@angular/forms';
 import {CheckUserService} from './check-user.service';
 import {RegistrationService} from './registration.service';
 import {UnitInfoService} from './home-page/unit-info.service';
 import {HomePageComponent} from './home-page/home-page.component';
+import {UnitOneComponent} from './unit-one/unit-one.component';
+import {PianoLessonComponent} from './unit-one/piano-lesson/piano-lesson.component';
+import {PianoLessonService} from './unit-one/piano-lesson/piano-lesson.service';
 
 
 @NgModule({
@@ -22,7 +25,9 @@ import {HomePageComponent} from './home-page/home-page.component';
     AppComponent,
     HeaderComponent,
     UserRegistrationComponent,
-    HomePageComponent
+    HomePageComponent,
+    UnitOneComponent,
+    PianoLessonComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +37,8 @@ import {HomePageComponent} from './home-page/home-page.component';
     AngularFireDatabaseModule,
     FormsModule
   ],
-  providers: [AuthenticationService, CheckUserService, RegistrationService, UnitInfoService],
+  providers: [AuthenticationService, CheckUserService, RegistrationService, UnitInfoService, PianoLessonService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
